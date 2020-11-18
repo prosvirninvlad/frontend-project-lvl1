655c1f57cb7e335af3324ec1482e20e7f599a9f6
Vladislav Prosvirnin <v.prosvirnin@ntechlab.com>
2020-11-18 23:56:49 +0300

    Refactoring of some brain games.

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import cli, { read } from '../src/cli.js';
+import { random } from '../src/common.js';
 import playBrainGame from '../src/games/brain.game.js';
 
-const MIN_RANDOM_VALUE = 1;
 const MAX_RANDOM_VALUE = 100;
 const ADD_OP_SIGN = '+';
 const SUB_OP_SIGN = '-';
@@ -14,14 +14,12 @@ const OPERATORS_MAP = {
   [MUL_OP_SIGN]: (numberA, numberB) => numberA * numberB,
 };
 
-const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
-
 playBrainGame({
   cli,
   instruction: 'What is the result of the expression?',
   round: () => {
-    const numberA = random(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
-    const numberB = random(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
+    const numberA = random(MAX_RANDOM_VALUE);
+    const numberB = random(MAX_RANDOM_VALUE);
     const operator = OPERATORS[random(0, OPERATORS.length)];
     const answer = parseInt(read(`Question: ${numberA} ${operator} ${numberB}\nYour answer:`), 10);
     const key = OPERATORS_MAP[operator](numberA, numberB);
---
+++ src/common.js
@@ -0,0 +1,2 @@
+export const isEven = (number) => !(number % 2);
+export const random = (max) => Math.floor(Math.random() * Math.floor(max));
