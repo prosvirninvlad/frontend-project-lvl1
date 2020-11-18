80cc3b74224ca6bdf2466bb06c954f20f65249dd
Vladislav Prosvirnin <v.prosvirnin@rosenergo.com>
2020-11-19 01:42:44 +0300

    Removed rest parameter from print function.

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import promptly from 'promptly';
 
 export const read = (prompt) => promptly.prompt(prompt && `${prompt} `);
-export const print = (...messages) => console.log(...messages);
+export const print = (message) => console.log(message);
 export async function readInt(prompt) {
   return parseInt(await read(prompt), 10);
 }
