280e3e0935d53babc2bfdacd61ef9de0e390568c
Vladislav Prosvirnin <v.prosvirnin@rosenergo.com>
2020-12-01 17:20:34 +0300

    Replaced break statement to return in playGame function.

--- src/engine.js
+++ src/engine.js
@@ -1,20 +1,17 @@
 const MAX_ROUNDS = 3;
 
 export default async function playGame(hooks) {
-  let rounds = 0;
   const data = await hooks.onStarted();
   const play = () => hooks.onRoundChange(data);
 
-  for (; rounds < MAX_ROUNDS; rounds += 1) {
+  for (let rounds = 0; rounds < MAX_ROUNDS; rounds += 1) {
     const round = await play();
     if (!round.passed) {
       hooks.onRoundFailed(data, round);
-      break;
+      return;
     }
     hooks.onRoundPassed(data, round);
   }
 
-  if (rounds === MAX_ROUNDS) {
-    hooks.onSucceed(data);
-  }
+  hooks.onSucceed(data);
 }
