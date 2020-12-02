7b9c0411dc446a3c48fcad7e63f360e6dd39761b
Vladislav Prosvirnin <v.prosvirnin@rosenergo.com>
2020-12-02 15:51:16 +0300

    Removed unused readInt function.

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,5 @@ import promptly from 'promptly';
 
 export const read = (prompt) => promptly.prompt(prompt && `${prompt} `);
 export const print = (message) => console.log(message);
-export async function readInt(prompt) {
-  return parseInt(await read(prompt), 10);
-}
+
 export default { print, read };
