70f2d5b82edffdc46754d9c6a472b9ebeceb4c76
Vladislav Prosvirnin <v.prosvirnin@rosenergo.com>
2020-12-05 14:59:35 +0300

    Removed internal helper functions in playBrainGame function.

--- src/engine.js
+++ src/engine.js
@@ -5,57 +5,21 @@ function isValidAnswer(answer, key) {
 }
 
 export default async function playBrainGame({ instruction, cli, generateGameRound }) {
-  function printGameName() {
-    cli.print('Welcome to the Brain Games!');
-  }
-
-  function printGameUserName(name) {
-    cli.print(`Hello, ${name}`);
-  }
-
-  function printGameInstruction() {
-    cli.print(instruction);
-  }
-
-  function readGameUserName() {
-    return cli.read('May I have your name?');
-  }
-
-  function printGameSucceedMessage(name) {
-    cli.print(`Congratulations, ${name}!`);
-  }
-
-  function readUserQuestionAnswer(question) {
-    return cli.read(`Question: ${question}\nYour answer:`);
-  }
-
-  function printGameRoundPassedMessage() {
-    cli.print('Correct!');
-  }
-
-  function printGameRoundFailedMessage(answer, key) {
-    cli.print(`'${answer}' is wrong answer ;(. Correct answer was '${key}'.`);
-  }
-
-  function printGameFailedMessage(name) {
-    cli.print(`Let's try again, ${name}!`);
-  }
-
-  printGameName();
-  const name = await readGameUserName();
-  printGameUserName(name);
-  printGameInstruction();
+  cli.print('Welcome to the Brain Games!');
+  const name = await cli.read('May I have your name?');
+  cli.print(`Hello, ${name}`);
+  cli.print(instruction);
 
   for (let rounds = 0; rounds < MAX_ROUNDS; rounds += 1) {
     const { question, key } = generateGameRound();
-    const answer = await readUserQuestionAnswer(question);
+    const answer = await cli.read(`Question: ${question}\nYour answer:`);
     if (!isValidAnswer(answer, key)) {
-      printGameRoundFailedMessage(answer, key);
-      printGameFailedMessage(name);
+      cli.print(`'${answer}' is wrong answer ;(. Correct answer was '${key}'.`);
+      cli.print(`Let's try again, ${name}!`);
       return;
     }
-    printGameRoundPassedMessage();
+    cli.print('Correct!');
   }
 
-  printGameSucceedMessage(name);
+  cli.print(`Congratulations, ${name}!`);
 }
