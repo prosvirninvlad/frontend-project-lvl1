6dd01fd0c1c3b89501aa6dc0a6708ef4f2d9b6cc
Vladislav Prosvirnin <v.prosvirnin@rosenergo.com>
2020-12-08 19:18:49 +0300

    Removed initial brain games module,

--- bin/brain-games.js
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env node
-import playBrainGcdGame from '../src/games/games.js';
-
-playBrainGcdGame();
--- src/games/games.js
+++
@@ -1,7 +0,0 @@
-import { print, read } from '../cli.js';
-
-export default async function playBrainGcdGame() {
-  print('Welcome to the Brain Games!');
-  const name = await read('May I have your name?');
-  print(`Hello, ${name}!`);
-}
